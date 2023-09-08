7af0eb718587f8f509f6df8614a25a28105c27dd
GnanaDanielP <Gnana.D@lapisdata.com>
2023-09-08 13:27:13 +0530

    python_training

---
+++ books_to_scrape_08_09_2023/books.py
@@ -0,0 +1,45 @@
+import scrapy
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    
+    start_urls = ["https://books.toscrape.com"]
+
+    def parse(self, response):
+        blocks=response.xpath('//ol/li')
+        source_url=response.url
+        
+        for block in blocks:
+            book_name=block.xpath('.//h3/a/@title').get('')
+            book_price=block.xpath('.//p[@class="price_color"]/text()').get('')
+            detail_url=block.xpath('.//div[@class="image_container"]/a/@href').get('')
+            detail_url=response.urljoin(detail_url)
+            yield scrapy.Request(url=detail_url, callback=self.parse_detail,dont_filter=True,cb_kwargs={"source_url":source_url,"book_name":book_name,'book_price':book_price})
+            
+            
+        next_page=response.xpath('//li[@class="next"]/a/@href').get('')
+        if next_page:
+            next_page=response.urljoin(next_page)
+            yield scrapy.Request(next_page,callback=self.parse)
+    
+        
+        
+    def parse_detail(self, response,source_url,book_name,book_price):
+        detail_pages=response.xpath('//table[@class="table table-striped"]')
+        
+        for detail_page in detail_pages:
+            product_information=detail_page.xpath('.//th/text()').getall()
+            info_details=detail_page.xpath('.//td/text()').getall()
+           
+            book_details = dict(zip(product_information,info_details))
+            item = {'source_url':source_url,'book_name':book_name,'book_price':book_price}
+            item = item|book_details
+            
+            yield item
+            
+    
+
+            
+        
+        
